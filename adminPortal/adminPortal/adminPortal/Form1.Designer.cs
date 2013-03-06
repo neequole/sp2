9fd2fd044897946e017342b5381e78d7ec2c26e1
Nicole I. Tibay <neequole@gmail.com>
2013-03-07 04:38:09 +0800

    version 2.1
    w/ sample web service

---
+++ adminPortal/adminPortal/adminPortal/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.msgBox = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.logBox1 = new System.Windows.Forms.ListBox();
+            this.bookingPanel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.bookingPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 505);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bookingPanel1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(673, 570);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Reader/Card Detect";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.msgBox);
+            this.groupBox2.Location = new System.Drawing.Point(45, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(592, 41);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "message";
+            // 
+            // msgBox
+            // 
+            this.msgBox.FormattingEnabled = true;
+            this.msgBox.Location = new System.Drawing.Point(6, 17);
+            this.msgBox.Name = "msgBox";
+            this.msgBox.Size = new System.Drawing.Size(580, 17);
+            this.msgBox.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(443, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(215, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(45, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(392, 346);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(513, 505);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 59);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Try Again";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // logBox1
+            // 
+            this.logBox1.FormattingEnabled = true;
+            this.logBox1.HorizontalScrollbar = true;
+            this.logBox1.Location = new System.Drawing.Point(691, 6);
+            this.logBox1.Name = "logBox1";
+            this.logBox1.Size = new System.Drawing.Size(482, 576);
+            this.logBox1.TabIndex = 3;
+            // 
+            // bookingPanel1
+            // 
+            this.bookingPanel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.bookingPanel1.Controls.Add(this.groupBox4);
+            this.bookingPanel1.Controls.Add(this.groupBox3);
+            this.bookingPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bookingPanel1.Name = "bookingPanel1";
+            this.bookingPanel1.Size = new System.Drawing.Size(673, 575);
+            this.bookingPanel1.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(14, 16);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(644, 105);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "User Information";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(15, 128);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(643, 276);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Bookings";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1185, 599);
+            this.Controls.Add(this.logBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Admin Portal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.bookingPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox logBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox msgBox;
+        private System.Windows.Forms.Panel bookingPanel1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ adminPortal/adminPortal/adminPortal/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//for HTTP REQUEST
+using System.Net;           
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int hContext = 0, retcode, hCard, ActiveProtocol;   //hContext -> handle for resource manager
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bookingPanel1.Hide();
+            button1.Enabled = false; //disable and hide button1
+            button1.Visible = false;
+            msgBox.Items.Insert(0, "NO CARD READER DETECTED");
+            check_reader();
+        }
+
+        private bool check_reader()
+        {
+
+        //SCardEstablishContext
+            logBox1.Items.Add("Calling SCardEstablishContext...");
+            retcode = ModWinsCard.SCardEstablishContext(ModWinsCard.SCARD_SCOPE_USER, 0, 0, ref hContext);
+            if (retcode != ModWinsCard.SCARD_S_SUCCESS)
+            {
+                logBox1.Items.Add(retcode + ": "+ ModWinsCard.GetScardErrMsg(retcode));
+                return false;
+            }
+            logBox1.Items.Add("Success!");
+
+            //SCardListReaders
+            logBox1.Items.Add("Calling SCardListReaders...");
+            int pcchReaders = 0;
+
+            // List PCSC card readers installed 
+            retcode = ModWinsCard.SCardListReaders(hContext, null, null, ref pcchReaders);
+
+            if (retcode != ModWinsCard.SCARD_S_SUCCESS)
+            {
+                logBox1.Items.Add(retcode + ": " + ModWinsCard.GetScardErrMsg(retcode));
+                return false;
+            }
+
+            byte[] ReaderList = new byte[pcchReaders];
+            byte[] sReaderGroup = new byte[0];
+
+            //Get the list of reader present again but this time add sReaderGroup, retData as 2rd & 3rd parameter respectively.
+            retcode = ModWinsCard.SCardListReaders(hContext, sReaderGroup, ReaderList, ref pcchReaders);
+            logBox1.Items.Add("Success!");
+            
+            //used to split null delimited strings into string arrays
+            char[] delimiter = new char[1];
+            delimiter[0] = Convert.ToChar(0);
+
+            //Convert retData(Hexadecimal) value to String 
+            string readerStr = System.Text.ASCIIEncoding.ASCII.GetString(ReaderList);
+            string[] pcReaders = readerStr.Split(delimiter);   //list of installed readers
+            
+            foreach (string readerName in pcReaders)
+            {
+                if (readerName != null && readerName.Length > 1)
+                {
+                    logBox1.Items.Add("Found: " + readerName);
+                    comboBox1.Items.Add(readerName);
+                }
+            }
+
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+            button1.Visible = true;     //Enable SCardConnect Button
+            button1.Enabled = true;
+            button2.Visible = false;    //Disable search reader Button 
+            button2.Enabled = false;
+            msgBox.Items.Insert(0, "Please place contact card of user to reader and Press OK.");
+            return true;
+            
+        }
+
+        private bool checkCard() {
+            logBox1.Items.Add("Calling SCardConnect...");
+            // Connect to the reader using hContext handle and obtain hCard handle  
+            retcode = ModWinsCard.SCardConnect(hContext, comboBox1.SelectedItem.ToString(), ModWinsCard.SCARD_SHARE_EXCLUSIVE, 0 | 1, ref hCard, ref ActiveProtocol);
+            if (retcode != ModWinsCard.SCARD_S_SUCCESS)
+            {
+                logBox1.Items.Add(retcode + ": " + ModWinsCard.GetScardErrMsg(retcode));
+                msgBox.Items.Insert(0, "Please place contact card of user to reader and Press OK.");
+                return false;
+            }
+            logBox1.Items.Add("Success!");
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkCard() == true) { 
+                //change form
+                //groupBox1.Hide();
+                bookingPanel1.Show();
+                //sample a http request to a web service
+                string sURL;
+                sURL = "http://www.microsoft.com";
+
+                //create WebRequest object
+                WebRequest wrGETURL;
+                wrGETURL = WebRequest.Create(sURL);
+                WebProxy myProxy = new WebProxy("myproxy", 80);
+                myProxy.BypassProxyOnLocal = true;
+
+                wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+
+                Stream objStream;
+                objStream = wrGETURL.GetResponse().GetResponseStream();
+
+                StreamReader objReader = new StreamReader(objStream);
+
+                string sLine = "";
+                int i = 0;
+
+                while (sLine != null)
+                {
+                    i++;
+                    sLine = objReader.ReadLine();
+                    if (sLine != null)
+                        logBox1.Items.Add(i + " " + sLine);
+                }
+                
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            check_reader();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+ 
+        
+    }
+}
